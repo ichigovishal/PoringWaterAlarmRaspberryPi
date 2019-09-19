ce7d82a581d98a523419dd0dc187ac11a6588f2c
ichigovishal <55552148+ichigovishal@users.noreply.github.com>
2019-09-19 22:32:35 +0530

    Add files via upload

---
+++ WaterPumpControl.py
@@ -0,0 +1,17 @@
+
+
+class _Controller:
+    def __init__(self):
+        pass
+    
+    def turn_ON(self):
+        turn_on = True
+        return turn_on
+    
+    def turn_OFF(self):
+        turn_off = True
+        return turn_off
+    
+    def clear(self):
+        fuck = True
+        return fuck
